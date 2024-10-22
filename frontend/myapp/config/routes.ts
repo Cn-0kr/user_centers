b45e53fcc07701535e15d330750fe847ee97db06
Cn-0kr <144128233+Cn-0kr@users.noreply.github.com>
2024-10-22 21:30:06 +0800

    v3.4
    增加路由，修改查看权限；
    修改管理界面

--- frontend/myapp/config/routes.ts
+++ frontend/myapp/config/routes.ts
@@ -12,10 +12,19 @@ export default [
     path: '/admin',
     name: '管理页',
     icon: 'crown',
+    /**
+     * 仅管理员可访问
+     */
     access: 'canAdmin',
+    /**
+     * 通过路径/admin就可以访问到Admin组件
+     */
+    component: './Admin',
     routes: [
-      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin', redirect: '/admin/user-manage' },
+      { path: '/admin/user-manage', name: '用户管理', component: './Admin/UserManage' },
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
+
     ],
   },
   { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
